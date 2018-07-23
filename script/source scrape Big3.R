1553cf87d4d1f703330b6fdb8c3505168346c05f
Maria Guideng <35015553+mguideng@users.noreply.github.com>
2018-07-23 16:36:45 -0700

    Add

---
+++ script/source scrape Big3.R
@@ -0,0 +1,26 @@
+## Set URL
+baseurl <- "https://www.glassdoor.com/Reviews/"
+sort <- ".htm?sort.sortType=RD&sort.ascending=true"
+totalreviews <- read_html(paste(baseurl, company, sort, sep="")) %>% 
+  html_nodes(".margBot.minor") %>% 
+  html_text() %>% 
+  sub(" reviews", "", .) %>% 
+  sub(",", "", .) %>% 
+  as.integer()
+maxresults <- trunc((totalreviews/10)+1)     #whole number, and +1 to capture all
+
+## A. Create df by scraping: Date, Summary, Title, Pros, Cons, Helpful
+df.z <- map_df(1:maxresults, function(i) {
+  
+  cat("! ")     #progress indicator
+  
+  pg <- read_html(sprintf(paste(baseurl, company, "_P", i, sort, sep="")))
+  
+  data.frame(rev.date = html_text(html_nodes(pg, ".date.subtle.small, .featuredFlag")),
+             rev.sum = html_text(html_nodes(pg, ".reviewLink .summary:not([class*='hidden'])")),
+             rev.title = html_text(html_nodes(pg, "#ReviewsFeed .hideHH")),
+             rev.pros = html_text(html_nodes(pg, "#ReviewsFeed .pros:not([class*='hidden'])")),
+             rev.cons = html_text(html_nodes(pg, "#ReviewsFeed .cons:not([class*='hidden'])")),
+             rev.helpf = html_text(html_nodes(pg, ".tight")),
+             stringsAsFactors=F)
+})
